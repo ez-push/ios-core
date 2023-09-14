e74d1c431787cb210901ee8008ff89bf7e379c61
Julian Rittmann <julian.rittmann@rtl-extern.de>
2023-09-14 15:14:39 +0200

    added Package.swift

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version: 5.7
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "EZPushCore",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "EZPushCore",
+            targets: ["EZPushCore"]
+        ),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "EZPushCore",
+            url: "https://sdk.ez-push.cloud/ios/core/1.3.13/EZPushCore.zip",
+            checksum: "530b1a08e56ab651cb036fcbe641e1f6096053dba6c45a3dee20593213214aac"
+        )
+    ]
+)
