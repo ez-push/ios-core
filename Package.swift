3d193a026423de13542f97d5b92f5408f083a55a
addmore mobile <addmoremobile@addmores-Mac-mini.local>
2024-06-09 22:43:27 +0200

    chore: version bump for v3.0.1

--- Package.swift
+++ Package.swift
@@ -17,8 +17,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "EZPushCore",
-            url: "https://sdk.ez-push.cloud/ios/core/3.0.0/EZPushCore.zip",
-            checksum: "72aabad9bc6a59b2d4f8dbc76c7c4eb1cc3da67f848722fb22180b1cd77b657b"
+            url: "https://sdk.ez-push.cloud/ios/core/3.0.1/EZPushCore.zip",
+            checksum: "04dd528c8697e8def9009524357fe617858bcfd4d2098c918272d32341be8dd1"
         )
     ]
 )
